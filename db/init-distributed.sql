556ec05e79835c5af0f846e5cc590f2d8443b2b8
GwanWingYan <47978664+GwanWingYan@users.noreply.github.com>
2024-06-14 16:43:11 +0800

    fix: 修复mysql数据库中文乱码问题

--- db/init-distributed.sql
+++ db/init-distributed.sql
@@ -1,3 +1,13 @@
+SET character_set_client = utf8mb4;
+SET character_set_connection = utf8mb4;
+SET character_set_database = utf8mb4;
+SET character_set_results = utf8mb4;
+SET character_set_server = utf8mb4;
+SET collation_connection = utf8mb4_unicode_ci;
+SET collation_database = utf8mb4_unicode_ci;
+SET collation_server = utf8mb4_unicode_ci;
+
+
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `12306_ticket` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 SOURCE /opt/sql/schema-ticket.sql;
 SOURCE /opt/sql/data-ticket.sql;
