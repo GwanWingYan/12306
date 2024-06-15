f9251c296018a560c63e7222ecdfda5531b61aac
GwanWingYan <47978664+GwanWingYan@users.noreply.github.com>
2024-06-15 17:36:26 +0800

    fix: 修复网关 Token 验证黑白名单问题

--- services/gateway-service/src/main/java/org/opengoofy/index12306/biz/gatewayservice/config/Config.java
+++ services/gateway-service/src/main/java/org/opengoofy/index12306/biz/gatewayservice/config/Config.java
@@ -23,19 +23,12 @@ import java.util.List;
 
 /**
  * 过滤器配置
- *
- * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
  */
 @Data
 public class Config {
 
     /**
-     * 白名单路径
-     */
-    private List<String> whitePathList;
-
-    /**
      * 黑名单前置路径
      */
-    private List<String> blackPathPreList;
+    private List<String> blackPathPre;
 }
