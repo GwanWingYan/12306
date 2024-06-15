60e2c6a76f06b73c0510ebac07635660ff940c58
GwanWingYan <47978664+GwanWingYan@users.noreply.github.com>
2024-06-15 22:14:40 +0800

    feature: 购票服务引入日志打印组件库

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/controller/TicketController.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/controller/TicketController.java
@@ -25,8 +25,13 @@ import org.opengoofy.index12306.biz.ticketservice.dto.resp.TicketPurchaseRespDTO
 import org.opengoofy.index12306.biz.ticketservice.remote.dto.PayInfoRespDTO;
 import org.opengoofy.index12306.biz.ticketservice.service.TicketService;
 import org.opengoofy.index12306.framework.starter.convention.result.Result;
+import org.opengoofy.index12306.framework.starter.log.annotation.ILog;
 import org.opengoofy.index12306.framework.starter.web.Results;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 车票控制层
@@ -48,6 +53,7 @@ public class TicketController {
     /**
      * 购买车票
      */
+    @ILog
     @PostMapping("/api/ticket-service/ticket/purchase")
     public Result<TicketPurchaseRespDTO> purchaseTickets(@RequestBody PurchaseTicketReqDTO requestParam) {
         return Results.success(ticketService.purchaseTickets(requestParam));
