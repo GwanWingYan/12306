e5a26f032ace1aeaef3ce73eb0d9c7777529a173
GwanWingYan <47978664+GwanWingYan@users.noreply.github.com>
2024-06-17 22:06:40 +0800

    feature: 引入 Crane4j 翻译框架节省状态值的编码和名称转换工作

--- services/order-service/src/main/java/org/opengoofy/index12306/biz/orderservice/common/enums/OrderItemStatusEnum.java
+++ services/order-service/src/main/java/org/opengoofy/index12306/biz/orderservice/common/enums/OrderItemStatusEnum.java
@@ -17,45 +17,50 @@
 
 package org.opengoofy.index12306.biz.orderservice.common.enums;
 
+import cn.crane4j.annotation.ContainerEnum;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
  * 订单明细状态枚举
  */
+@ContainerEnum(namespace = "OrderItemStatusEnum", key = "status", value = "statusName")
 @RequiredArgsConstructor
 public enum OrderItemStatusEnum {
 
     /**
      * 待支付
      */
-    PENDING_PAYMENT(0),
+    PENDING_PAYMENT(0, "待支付"),
 
     /**
      * 已支付
      */
-    ALREADY_PAID(10),
+    ALREADY_PAID(10, "已支付"),
 
     /**
      * 已进站
      */
-    ALREADY_PULL_IN(20),
+    ALREADY_PULL_IN(20, "已进站"),
 
     /**
      * 已取消
      */
-    CLOSED(30),
+    CLOSED(30, "已取消"),
 
     /**
      * 已退票
      */
-    REFUNDED(40),
+    REFUNDED(40, "已退票"),
 
     /**
      * 已改签
      */
-    RESCHEDULED(50);
+    RESCHEDULED(50, "已改签");
 
     @Getter
-    private final int status;
+    private final Integer status;
+
+    @Getter
+    private final String statusName;
 }
