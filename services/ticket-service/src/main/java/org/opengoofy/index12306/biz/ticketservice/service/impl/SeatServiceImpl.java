1590d4efc29f3403d92db00021ea4bd529c48f82
GwanWingYan <47978664+GwanWingYan@users.noreply.github.com>
2024-06-17 19:39:15 +0800

    feature: 开发查询列车有余票的车厢号集合

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/service/impl/SeatServiceImpl.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/service/impl/SeatServiceImpl.java
@@ -42,6 +42,7 @@ import java.util.stream.Collectors;
 import static org.opengoofy.index12306.biz.ticketservice.common.constant.RedisKeyConstant.TRAIN_STATION_CARRIAGE_REMAINING_TICKET;
 
 /**
+ * 座位接口层实现
  */
 @Service
 @RequiredArgsConstructor
@@ -81,6 +82,20 @@ public class SeatServiceImpl extends ServiceImpl<SeatMapper, SeatDO> implements 
                 .endStation(arrival)
                 .build();
         return seatMapper.listSeatRemainingTicket(seatDO, trainCarriageList);
+    }
+
+    @Override
+    public List<String> listUsableCarriageNumber(String trainId, Integer carriageType, String departure, String arrival) {
+        LambdaQueryWrapper<SeatDO> queryWrapper = Wrappers.lambdaQuery(SeatDO.class)
+                .eq(SeatDO::getTrainId, trainId)
+                .eq(SeatDO::getSeatType, carriageType)
+                .eq(SeatDO::getStartStation, departure)
+                .eq(SeatDO::getEndStation, arrival)
+                .eq(SeatDO::getSeatStatus, SeatStatusEnum.AVAILABLE.getCode())
+                .groupBy(SeatDO::getCarriageNumber)
+                .select(SeatDO::getCarriageNumber);
+        List<SeatDO> seatDOList = seatMapper.selectList(queryWrapper);
+        return seatDOList.stream().map(SeatDO::getCarriageNumber).collect(Collectors.toList());
     }
 
     @Override
