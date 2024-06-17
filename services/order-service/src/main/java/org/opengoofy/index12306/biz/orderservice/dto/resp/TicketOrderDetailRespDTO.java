39ae57e26e64542622bd65d16335485023ca16d9
GwanWingYan <47978664+GwanWingYan@users.noreply.github.com>
2024-06-17 17:13:20 +0800

    feature: 订单分页列表返回订单号

--- services/order-service/src/main/java/org/opengoofy/index12306/biz/orderservice/dto/resp/TicketOrderDetailRespDTO.java
+++ services/order-service/src/main/java/org/opengoofy/index12306/biz/orderservice/dto/resp/TicketOrderDetailRespDTO.java
@@ -30,6 +30,11 @@ import java.util.List;
 public class TicketOrderDetailRespDTO {
 
     /**
+     * 订单号
+     */
+    private String orderSn;
+
+    /**
      * 出发站点
      */
     private String departure;
