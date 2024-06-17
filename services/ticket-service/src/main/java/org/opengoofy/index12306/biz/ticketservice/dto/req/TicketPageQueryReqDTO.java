083b7a2fa811c1ac5eb1a012b89b3aae4e38f005
GwanWingYan <47978664+GwanWingYan@users.noreply.github.com>
2024-06-17 18:26:01 +0800

    make nvm Update (TicketPageQueryReqDTO): 修改出发地、目的地字段注释

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/dto/req/TicketPageQueryReqDTO.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/dto/req/TicketPageQueryReqDTO.java
@@ -30,12 +30,12 @@ import java.util.Date;
 public class TicketPageQueryReqDTO extends PageRequest {
 
     /**
-     * 出发地
+     * 出发地 Code
      */
     private String fromStation;
 
     /**
-     * 目的地
+     * 目的地 Code
      */
     private String toStation;
 
